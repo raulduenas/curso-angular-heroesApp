1f140d820b7ed5aa1a868ea69a3b8f4837a8d388
Raúl Dueñas <raulduenas29@gmail.com>
2021-08-16 23:23:57 -0500

    Fin de la sección 14

--- src/app/heroes/pipes/imagen.pipe.ts
+++ src/app/heroes/pipes/imagen.pipe.ts
@@ -1,13 +1,22 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { publishReplay } from 'rxjs/operators';
 import { Heroe } from '../interfaces/heroes.interface';
 
 @Pipe({
   name: 'imagen'
+  //pure: true
 })
 export class ImagenPipe implements PipeTransform {
 
   transform(value: Heroe): string {
-    return `assets/heroes/${value.id}.jpg`;
+
+    if (!value.id  && !value.alt_img ) {
+      return 'assets/no-image.png';
+    } else if ( value.alt_img ) {
+      return value.alt_img;
+    } else {
+      return `assets/heroes/${value.id}.jpg`;
+    }
   }
 
 }
